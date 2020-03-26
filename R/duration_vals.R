9a590bd678bf03e9db6860760896a9d6c968148f
Hannah Weeks <hannahlweeks@gmail.com>
2020-03-26 13:49:59 -0500

    create duration and route dictionaries

---
+++ R/duration_vals.R
@@ -0,0 +1,12 @@
+#' Keywords Specifying Duration
+#'
+#' Vector of keywords indicating duration of a drug, i.e., how long the patient should
+#' remain on the specified drug regimen. The form of each duration is given as a regular expression.
+#'
+#' @format A vector with X character strings.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(duration_vals)
+"duration_vals"
---
+++ R/route_vals.R
@@ -0,0 +1,12 @@
+#' Keywords Specifying Route
+#'
+#' Vector of keywords indicating route of administration for a drug.
+#' The form of each route is given as a regular expression.
+#'
+#' @format A vector with X character strings.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(route_vals)
+"route_vals"
